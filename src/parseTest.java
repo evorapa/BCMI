f064a9f3b60049a7e6562d10e3f454c86c64e232
zrolston <zrolston@ufl.edu>
2016-10-18 19:22:43 -0400

    Set up for averaging and implementation of audio system

--- src/parseTest.java
+++ src/parseTest.java
@@ -4,6 +4,10 @@ public class parseTest {
 	static int recPort = 5000;
 	float museCon;
 	float calcCon;
+	float alpha;
+	float beta;
+	float theta;
+	boolean useable;
 	OscP5 museServer;
 	
 	static parseTest parser;
@@ -17,32 +21,51 @@ public class parseTest {
 		p.museServer.plug(p, "valid", "/muse/elements/jaw_clench");
 		p.museServer.plug(p, "valid", "/muse/elements/is_good");
 		
+		/*
+		while(true){
+			System.out.println("From the muse: " + p.museCon);
+			System.out.println("From calcs: " + p.calcCon);
+		}*/
+		
 	}
 	
 	public void museCon(float f){
-		System.out.println("PLUG");
-		System.out.println("Plug for museCon: " + f);
+		//System.out.println("PLUG");
+		//System.out.println("Plug for museCon: " + f);
+		if(useable)
+			museCon = f;
 	}
 	
 	public void valid(int b){
-		System.out.println("PLUGGED");
-		System.out.println("Plug for blink/jaw_clench: " + b);
+		if(b != 0){
+			useable = false;
+		}
 	}
 	
 	public void valid(int a, int b, int c, int d){
-		System.out.println("PLUGGED");
-		System.out.println("Plug for is_good: " + a + " " + b + " " + c + " " + d);
+		if((a+b+c+d) != 4){
+			useable = false;
+		}
+		else{
+			useable = true;
+		}
 	}
 	
 	void oscEvent(OscMessage msg) {
-		//System.out.println(msg.isPlugged());
-		/*
-		if (msg.checkAddrPattern("/muse/elements/experimental/concentration")==true && !msg.isPlugged()) {  
-			this.museCon = msg.get(0).floatValue();
-			System.out.println("### got a message " + msg);
-			System.out.print("Concentration: " + museCon + "\n"); 
-			
-		} 
-		*/
+		//System.out.println(msg);
+		
+		
+		if(useable){	
+			if (msg.checkAddrPattern("/muse/elements/alpha_absolute")==true) {  
+				this.alpha = (msg.get(0).floatValue() + msg.get(1).floatValue() + msg.get(2).floatValue() + msg.get(3).floatValue())/4; 
+			} 
+			if (msg.checkAddrPattern("/muse/elements/beta_absolute")==true) {  
+				this.beta = (msg.get(0).floatValue() + msg.get(1).floatValue() + msg.get(2).floatValue() + msg.get(3).floatValue())/4;
+			} 
+			if (msg.checkAddrPattern("/muse/elements/theta_absolute")==true) {  
+				this.theta = (msg.get(0).floatValue() + msg.get(1).floatValue() + msg.get(2).floatValue() + msg.get(3).floatValue())/4;
+				this.calcCon = (beta/(alpha+theta));
+			} 
+		}
 	}
 }
